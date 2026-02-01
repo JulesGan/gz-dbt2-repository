569fa8ea2405eea13e03663d8e52431ee4f9257b
Jules Ganem <jganem.osteo@gmail.com>
2026-02-01 12:40:37 +0000

    int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,7 +4,9 @@ orders_id,
 s.products_id,
 revenue, 
 quantity, 
-p.purchase_price 
+p.purchase_price, 
+ROUND(s.quantity*p.purchase_price,2) as purchase_cost,
+ROUND(s.revenue-(s.quantity*p.purchase_price),2) as margin
 FROM {{ ref('stg_raw__sales') }} as s
 LEFT JOIN {{ ref('stg_raw__product') }} as p 
 USING(products_id)
